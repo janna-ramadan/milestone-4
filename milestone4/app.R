a6494ac45b9b0cfd741c50c0812438c5da9a2c81
“Janna <jannaramadan@jannas-mbp.lan>
2020-10-16 15:53:31 -0400

    2 data sets in

--- milestone4/app.R
+++ milestone4/app.R
@@ -48,3 +48,54 @@ server <- function(input, output) {
 # Run the application 
 shinyApp(ui = ui, server = server)
 
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- navbarPage(
+    "Milestone 4",
+    tabPanel("Model",
+             fluidPage(
+                 titlePanel("Model Title"),
+                 sidebarLayout(
+                     sidebarPanel(
+                         textInput("name", "What's your name?"),  
+                         textOutput("greeting"),
+                         sliderInput("year", label="When were you born?",   
+                                     min = 1960, max = 2020, value = 2000),  
+                         textOutput("AgeCalc"),
+                         selectInput(
+                             "plot_type",
+                             "Plot Type",
+                             c("Option A" = "a", "Option B" = "b")
+                             
+                         )),
+                     mainPanel())
+             )),
+    tabPanel("Discussion",
+             titlePanel("Discussion Title"),
+             p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
+    tabPanel("About", 
+             titlePanel("About"),
+             h3("Project Background and Motivations"),
+             p("Hello, this is my Milestone 4. In it I discuss the various sources 
+               I am thinking of using for my project on the intersection of social media, 
+               newspapers, and legislation on national public opinion towards Muslims, 
+               with a potential impact measure on Arab and South Asian communities caught 
+               in the stereotypes surrounding Muslims."),
+             h3("About Me"),
+             p("My name is Janna Ramadan and I study Government and Near Eastern Languages and Civilizations. 
+             You can reach me at janna_ramadan@college.harvard.edu.")))
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    output$greeting<- renderText({paste0("Hello ", input$name)})
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
